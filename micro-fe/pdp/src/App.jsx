6d9d7390ba92c6a61d47ef980d17ddbc8eb13065
Nhan Phan <phannhan.dn@gmail.com>
2023-11-24 00:38:30 +0700

    Update

--- micro-fe/pdp/src/App.jsx
+++ micro-fe/pdp/src/App.jsx
@@ -1,32 +1,39 @@
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 // import SafeComponent from "./SafeComponent";
 import Header from 'home/Header'
 // const Header = React.lazy(() => import('home/Header'))
 import Footer from 'home/Footer'
+import PDPContent from "./PDPContent";
+
 
 import "./index.scss";
 
 const App = () => {
 
   return (
-    <div className="mt-10 text-3xl mx-auto max-w-6xl">
-      {/* <Suspense fallback={<div>Loading</div>}>
+    <Router >
+      <div className="mt-10 text-3xl mx-auto max-w-6xl">
+        {/* <Suspense fallback={<div>Loading</div>}>
           <Header />
         </Suspense> */}
 
-      {/* Handle Error */}
-      {/* <SafeComponent>
+        {/* Handle Error */}
+        {/* <SafeComponent>
         <Header />
       </SafeComponent> */}
 
-      <Header />
-      <div className="my-10">
-        PDP Page Content
+        <Header />
+        <div className="my-10">
+          <Routes>
+            <Route path="/product/:id" element={<PDPContent />} />
+          </Routes>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </Router>
   )
 };
 ReactDOM.render(<App />, document.getElementById("app"));
